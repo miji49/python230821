53a1fac8d4a42db748663335125ffa8e1a48c069
miji49 <miji49@hanmail.net>
2023-08-23 11:38:57 +0900

    정규표현식 연습

---
+++ demoRe.py
@@ -0,0 +1,19 @@
+# demoRe.py
+
+import re
+
+result = re.search("[0-9]*th", "  35th")
+print(result)
+print(result.group())
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+print("---연도를 찾는 경우---")
+result = re.search("\d{4}", "올해는 2023년입니다.")
+print(result.group())
+print("---우편번호를 찾는 경우")
+result = re.search("\d{5}", "우리동네는 532000입니다.")
+print(result.group())
+
+
